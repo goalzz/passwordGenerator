4ba55c47b4543cff45bbdc90b52ad310fff45411
Edona Ahmetaj <edonamasur@gmail.com>
2023-11-24 21:36:59 +0000

    added code to copy the password when password is generated and an alert to notify user for it

--- starter/script.js
+++ starter/script.js
@@ -11,14 +11,13 @@ var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
 
 // We declare the function getPasswordOptions here:
 function getPasswordOptions() {
-   // 'prompt' will show a dialog box on the screen with the message we set. parseInt will convert the user's input (which is initially a string) into an integer.
+   // 'prompt' will show a dialog box on the screen with the message we set. parseInt will convert the user's input into an integer.
   var length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters)")); 
 
   // if the answer is not a number, the user will receeive a message
   if (isNaN(length) || length < 8 || length > 128) {
     alert("Please enter a valid number between 8 and 128.");
     return null;
-}
 }
 
 // Confirm use of special characters
@@ -38,3 +37,89 @@ if (!includeSpecial && !includeNumeric && !includeLowercase && !includeUppercase
   alert("Please select at least one character type.");
   return null; // return null if none selected
 }
+
+// the function will return an object with user's input
+return {
+  length: length,
+  special: includeSpecial,
+  numeric: includeNumeric,
+  lower: includeLowercase,
+  upper: includeUppercase
+};
+}
+
+// Declaring the function for getting a random element from an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  return randElement;
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+
+  if (!options) {
+    return ""; // User canceled or entered invalid input
+  }
+
+  // Concatenate arrays based on user input
+  var allCharacters = [];
+  if (options.special) {
+    allCharacters = allCharacters.concat(specialCharacters);
+  }
+  if (options.numeric) {
+    allCharacters = allCharacters.concat(numericCharacters);
+  }
+  if (options.lower) {
+    allCharacters = allCharacters.concat(lowerCaseCharacters);
+  }
+  if (options.upper) {
+    allCharacters = allCharacters.concat(upperCaseCharacters);
+  }
+
+  // Initializing the password as an empty string
+  var password = "";
+
+  for (var i = 0; i < options.length; i++) {
+    var randomChar = getRandom(allCharacters);
+    password += randomChar;
+  }
+  return password;
+}
+
+// after this code is executed, the generateBtn variable can be used in the JavaScript code to interact with the HTML button element that has the id "generate."
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// We add the event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+// Declaring a function to copy the generated password to the clipboard
+function copyPassword() {
+  // Select the text area containing the password
+  var passwordText = document.querySelector('#password');
+  passwordText.select();
+  
+  // code to copy the selected text to the clipboard
+  document.execCommand('copy');
+  
+  // Deselecting the text area
+  window.getSelection().removeAllRanges();
+
+  // Alerting the user after copying successful:
+  alert('Password copied to clipboard!');
+}
+
+var generateBtn = document.querySelector('#generate');
+var copyBtn = document.querySelector('#copy');
+
+// Adding the event listener for copying the code
+copyBtn.addEventListener('click', copyPassword);
